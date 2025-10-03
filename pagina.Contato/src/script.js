648ea21a855674f8db0c5281fc54f77298283af8
Geovanna Cristina dos Santos <geovanna.cristina2014@gmail.com>
2025-10-03 12:59:20 -0300

    colocar a nav em tudo

--- pagina.Contato/src/script.js
+++ pagina.Contato/src/script.js
@@ -2,12 +2,7 @@
     window.location.href = '/pagina.login/login/index.html'; // Aqui vai a URL que você quer abrir
  });
 
-  document.getElementById('peca.aqui').addEventListener('click', () => {
-    window.location.href = '/pagina.menu/index.html'; // Aqui vai a URL que você quer abrir
- });
-
-
-function abrirModal() {
+ function abrirModal() {
     document.getElementById('minhaModal').style.display = 'flex';
   }
 
